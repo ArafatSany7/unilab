08e30392b292f98e87859dd0a5b466664414ed63
ArafatSany7 <human.sany7@gmail.com>
2025-10-23 01:52:35 +0600

    arr

---
+++ Js-Practice/linear.js
@@ -0,0 +1,19 @@
+function lin(arr, target) {
+  for (let i = 0; i < number.length; i++) {
+    if (arr[i] == target) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+let number = [34, 45, 65, 33, 60, 71];
+let target = 33;
+
+let result = lin(number, target);
+
+if (result != -1) {
+  console.log(`Element found at index numner : ${result}`);
+} else {
+  console.log("elemetnt not found");
+}
