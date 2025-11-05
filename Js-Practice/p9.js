e324c07b44d5f1ab246ca6ff0896e418c2c7c978
ArafatSany7 <human.sany7@gmail.com>
2025-11-05 20:26:46 +0600

    p9

---
+++ Js-Practice/p9.js
@@ -0,0 +1,11 @@
+function calculateArea(width, height) {
+  let area = width * height;
+  return area;
+}
+
+width = 8;
+height = 10;
+
+let result = calculateArea(width, height);
+
+console.log(result);
